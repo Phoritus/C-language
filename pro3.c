3f09c720d518e9fedc55746be9949cbf8a16700a
Poookk <84915497+Poookk@users.noreply.github.com>
2024-11-24 20:01:12 +0700

    arithmetic operators

---
+++ pro3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+    // arithmetic operators
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // ++ increment
+    // -- decrement
+    int x=16;
+    int y=8;
+    
+    //int z=x+y;
+    //float z=x/(float)y;
+    //int z=x%y;
+    x++;
+    y--;
+    //printf("%d\n",x);printf("%d",y);
+
+
+    // การย่อสมการ การคิดเลขเช่น x = x+1 ย่อได้เป็น x+=1 ประมาณนี้
+    int a =10;
+    // a+=2;
+    // a-=2;
+    // a*=2;
+    // a/=2;
+    // a%=2;
+    printf("%d",a);
+    return 0;
+}
+
+
+
