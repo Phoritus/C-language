0542c1eb87ef83a6465fd04348e93e0931e745ed
Poookk <84915497+Poookk@users.noreply.github.com>
2024-11-24 20:03:24 +0700

    variable type

---
+++ pro5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double a=sqrt(9);
+    double b=pow(2,4);
+    int c=round(3.56);
+    int e=ceil(3.14);
+    int d=floor(3.99);
+    double g=fabs(-13);
+    double f=log(3);
+    double h=sin(45);
+    double I=cos(45);
+    double j=tan(45);
+    printf("%lf",j);
+    return 0;
+}
