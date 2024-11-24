ff84a0f537224aee6d07232906a868f3e37e43e4
Poookk <84915497+Poookk@users.noreply.github.com>
2024-11-24 19:57:19 +0700

    declare variable and print form

---
+++ pro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+    float a= 3.1415926535897932;
+    double b=3.1415926535897932;int j=23;
+    printf("%d\n",j);
+    /* printf("%.15f\n",a);
+    printf("%.15lf",b); */
+    bool e=false;
+    char name[]="Ming";printf("%s",name);
+    //printf("%d\n",e);
+    char z=120;
+    //printf("%c",z);
+}
