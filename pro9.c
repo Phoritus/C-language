0f0218ca0e114802d057a4c5bedb68fdf329050c
Poookk <84915497+Poookk@users.noreply.github.com>
2024-11-24 20:05:05 +0700

    function in C

---
+++ pro9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void fuckbirth(){
+    printf("Happy fuck birth day\n");
+    printf("fuck day\n");
+    printf("Happy fuck birth fuck day\n");
+}
+int main (){
+    fuckbirth();
+    fuckbirth();
+    fuckbirth();
+    return 0;
+}
